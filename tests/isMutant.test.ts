702f2a1982a2788010c06ffee10204479ccdb973
Camila Bustos <camilabustos@pop-os.localdomain>
2022-06-13 12:22:59 -0400

    :white_check_mark: add isMutant test

---
+++ tests/isMutant.test.ts
@@ -0,0 +1,14 @@
+import mutant from '../src/helpers/getIsMutant';
+
+describe('POST /mutant', () => { 
+  it('should respond false', () => {
+    const humanDna = ['ATGTCC', 'ACGTGC', 'TACGAT', 'CGCCGG', 'CCACTC', 'TCCACC']
+    const isMutant = mutant(humanDna);
+    expect(isMutant).toBe(false);
+  });
+  it('should respond true', () => {
+    const mutantDna = ["ATGCGA","CAGTGC","TTATGT","AGAAGG","CCCCTA","TCACTG"]
+    const isMutant = mutant(mutantDna);
+    expect(isMutant).toBe(true);
+  });
+});
